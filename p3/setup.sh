750fba3c254db54ef77aa8d7aa75b6e3eaaa2053
Pierre louis GOUDET <pgoudet@paul-f4Br3s5.clusters.42paris.fr>
2023-06-29 11:54:05 +0200

    last commit

--- p3/setup.sh
+++ p3/setup.sh
@@ -18,6 +18,8 @@ then
 fi
 
 k3d cluster create pgoudet --port 8080:80@loadbalancer --port 8443:443@loadbalancer --port 8888:8888@loadbalancer --subnet "172.20.0.0/16"
+# cette ligne expose un cluster avec des reidrection de port (ex 8080 a lexterieur du cluster enverra sur le port 80 a l interieur du cluster)
+
 
 READY=$(kubectl get deployment metrics-server -n kube-system)
 READY=${READY:71:1}
@@ -29,18 +31,24 @@ do
     READY=${READY:71:1}
     echo "metrics-server status READY = $READY"
 done
+# ce bloc attend juste que le service metrics-server soit en route (pour eviter les warning)
+
 
 kubectl create namespace argocd
+# on creer un namespace argocd pour linstaller
+# les sleep servent a temporiser car la creation des namespace et des service est tres longue
 
 sleep 1
 kubectl apply -f install-argocd.yaml -n argocd
-# sleep 1
-# kubectl apply -f config-argocd.yaml -n argocd
+# on install argocd a partir d un fichier degueu de 17000 ligne que l'on a recuperer depuis le site officiel
+
 sleep 1
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+# onexpose le service en modifiant son type => il devient type LoadBalancer et donc il est expose a l'exterieur du cluster
 sleep 1
 kubectl apply -f ingress.yaml -n argocd
 sleep 15
+# on parametre les routes pour les redirections depuis de l'exterieur
 PASS=$(/usr/local/bin/argocd admin -n argocd initial-password --grpc-web)
 PASS=${PASS%%" "*}
 sleep 15
